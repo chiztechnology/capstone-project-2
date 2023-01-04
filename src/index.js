773b17f64863c853b52ab8526bdcca1c98586510
PrangonGhose <prangonghose@gmail.com>
2023-01-05 02:50:18 +0600

    Add dynamic functionalities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import { sports } from './modules/sports.js';
+import { getPlayer } from './modules/players.js';
+import { getLeagues } from './modules/leagues.js';
 import './style/styles.css';
+
+const sportsHeader = document.querySelector('#sports');
+const leaguesHeader = document.querySelector('#leagues');
+const playerHeader = document.querySelector('#players');
+
+window.addEventListener('DOMContentLoaded', () => {
+  sports();
+});
+
+sportsHeader.addEventListener('click', () => {
+  sports();
+});
+
+playerHeader.addEventListener('click', () => {
+  getPlayer();
+});
+
+leaguesHeader.addEventListener('click', () => {
+  getLeagues();
+});
