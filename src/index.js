7370e6cc988d97d3283e52c7f4af0fb4459511ad
PrangonGhose <prangonghose@gmail.com>
2023-01-08 12:40:38 +0600

    Update main JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import 'lodash'
+import 'lodash';
 import './style/styles.css';
 import { sports } from './modules/sports.js';
 import { getPlayer } from './modules/players.js';
 import { getLeagues } from './modules/leagues.js';
+
 const sportsHeader = document.querySelector('#sports');
 const leaguesHeader = document.querySelector('#leagues');
 const playerHeader = document.querySelector('#players');
